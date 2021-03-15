53cd263a4320c63d337cdd06a625f34156e74144
Antonio Cuadros <antculap@gmail.com>
2021-03-15 18:01:56 +0100

    closes #4 and closes #2 menu terminado, movimiento entre fragments completado

--- src/app/src/main/java/com/acl/leyendasdelaalhambra/MainActivity.kt
+++ src/app/src/main/java/com/acl/leyendasdelaalhambra/MainActivity.kt
@@ -2,10 +2,38 @@ package com.acl.leyendasdelaalhambra
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mapaFragment = MapaFragment()
+        val recorridosFragment = RecorridosFragment()
+        val listaLeyendasFragment = ListaLeyendasFragment()
+
+        cambiaFragment(mapaFragment, false)
+
+        menu_inferior.setOnNavigationItemSelectedListener{
+            when(it.itemId){
+                R.id.icono_mapa -> cambiaFragment(mapaFragment, true)
+                R.id.icono_recorridos -> cambiaFragment(recorridosFragment, true)
+                R.id.icono_lista_leyendas -> cambiaFragment(listaLeyendasFragment, true)
+            }
+            true
+        }
+    }
+
+    fun cambiaFragment(fragmento: Fragment, vuelta_atras:Boolean){
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.cambia_fragmento, fragmento)
+            commit()
+            if(vuelta_atras){
+                addToBackStack(null)
+            }
+        }
     }
 }
