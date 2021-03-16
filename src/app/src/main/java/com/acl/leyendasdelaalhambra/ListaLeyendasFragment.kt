24faff344789e5fd1ea1adcb16b1f16b2cf7aa31
Antonio Cuadros <antculap@gmail.com>
2021-03-16 15:11:17 +0100

    Avanza #10, fragmento listaleyendas recuperado onCreateView por defecto

--- src/app/src/main/java/com/acl/leyendasdelaalhambra/ListaLeyendasFragment.kt
+++ src/app/src/main/java/com/acl/leyendasdelaalhambra/ListaLeyendasFragment.kt
@@ -6,11 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class ListaLeyendasFragment : Fragment(R.layout.fragment_lista_leyendas) {
+class ListaLeyendasFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_lista_leyendas, container, false)
     }
+
 
 }
