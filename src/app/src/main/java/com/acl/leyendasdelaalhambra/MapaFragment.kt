053aca604eabdeafaa0fd63dbf66c0dcffd09e12
Antonio Cuadros <antculap@gmail.com>
2021-03-23 18:13:54 +0100

    avanza #16 ya que mapa funcionando

--- src/app/src/main/java/com/acl/leyendasdelaalhambra/MapaFragment.kt
+++ src/app/src/main/java/com/acl/leyendasdelaalhambra/MapaFragment.kt
@@ -5,12 +5,56 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.fragment_mapa.*
 
-class MapaFragment : Fragment(R.layout.fragment_mapa) {
+class MapaFragment : Fragment(), OnMapReadyCallback {
+    private lateinit var mapa: GoogleMap;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        var vista =  inflater.inflate(R.layout.fragment_mapa, container, false)
+
+        //inicializamos el fragmento
+        inicializaFragmento();
+
+        return vista;
+    }
+
+    private fun inicializaFragmento(){
+        val vista_fragmento = childFragmentManager.findFragmentById(R.id.mapa_fragmento) as SupportMapFragment;
+        //Genera el mapa de forma asíncrona
+        vista_fragmento.getMapAsync(this);
+        //Después de ejecutar lo anterior se pasa a ejecutar la función onMapReady
+    }
+
+    //se llama cuando se cargue el mapa
+    override fun onMapReady(_mapa: GoogleMap) {
+        mapa = _mapa;
+        centra_en_alhambra();
+    }
+
+    private fun centra_en_alhambra(){
+        val alhambra= LatLng(37.1760783, -3.5881413)
+        mapa.moveCamera(CameraUpdateFactory.newLatLngZoom(alhambra, 16F))
+        anadeMarcador(alhambra)
+    }
+
+    private fun anadeMarcador(coord:LatLng){
+        val coordenadas = coord;
+        val marcador = MarkerOptions().position(coordenadas).title("Alhambra");
+        mapa.addMarker(marcador)
+    }
 }
