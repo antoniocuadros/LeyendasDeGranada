031742f0b2d0348d6d5a1fd2b4a07018de587b57
Antonio Cuadros <antculap@gmail.com>
2021-03-24 09:45:10 +0100

    closes #17 ya que se piden permisos de ubicación y funcionando ubicación en tiempo real

--- src/app/src/main/java/com/acl/leyendasdelaalhambra/MapaFragment.kt
+++ src/app/src/main/java/com/acl/leyendasdelaalhambra/MapaFragment.kt
@@ -1,10 +1,22 @@
 package com.acl.leyendasdelaalhambra
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.checkSelfPermission
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -43,6 +55,7 @@ class MapaFragment : Fragment(), OnMapReadyCallback {
     //se llama cuando se cargue el mapa
     override fun onMapReady(_mapa: GoogleMap) {
         mapa = _mapa;
+        activarUbicacionTiempoReal()
         centra_en_alhambra();
 
         val accesoDatos = AccesoDatos()
@@ -66,6 +79,57 @@ class MapaFragment : Fragment(), OnMapReadyCallback {
         for(leyenda in leyendas){
             val coordenada = LatLng(leyenda.Lat,leyenda.Long);
             anadeMarcador(coordenada, leyenda.nombre)
+        }
+    }
+
+    private fun compruebaPermisos(context:Context){
+        //Desde android M necesitamos pedir explicitamente el permiso de ubicación
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+
+
+            //El permiso de ubicación ya está otorgado
+            if(ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                //OBTENEMOS LA UBICACIÓN
+            }
+            else{ //el permiso de ubicación no está otorgado
+                //pedimos el permiso, pasamos el permiso y un código cualquiera
+                requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 200)
+            }
+        }
+        else{ //inferior a android M, obtenemos la ubicación directamente
+            //OBTENEMOS LA UBICACIÓN
+        }
+    }
+
+    ///////////////////////////////////////////
+    //
+    //  PERMISOS Y UBICACIÓN EN TIEMPO REAL
+    //
+    ///////////////////////////////////////////
+    private fun activarUbicacionTiempoReal(){
+        //Si entra en el if es porque ya ha aceptado los permisos
+        if(ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            mapa.isMyLocationEnabled = true
+        }
+        else{ //aun no ha aceptado los permisos, tenemos que pedirle los permisos
+            //ya le hemos pedido los permisos con anterioridad pero los rechazó
+            if(ActivityCompat.shouldShowRequestPermissionRationale(activity as Activity, Manifest.permission.ACCESS_FINE_LOCATION)){
+                Toast.makeText(context, "Es necesario otorgar permisos de localización", Toast.LENGTH_LONG).show()
+            }
+            else{ //Por primera vez le pedimos los permisos
+                ActivityCompat.requestPermissions(activity as Activity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 200)
+            }
+        }
+    }
+
+    //Ahora tenemos que capturar la respuesta cuando el usuario acepte los permisos
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if(requestCode == 100){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){  //Ha aceptado el permiso
+                mapa.isMyLocationEnabled = true
+            }else{//se ha rechazado el permiso de ubicación
+                Toast.makeText(context, "Es necesario otorgar permisos de localización", Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
