4c98bc7238588e7e91f229126be7afd2e7b576dc
Antonio Cuadros <antculap@gmail.com>
2021-03-29 22:20:59 +0200

    Manejada la vuelta atrás en la app

--- src/app/src/main/java/com/acl/leyendasdelaalhambra/MapaFragment.kt
+++ src/app/src/main/java/com/acl/leyendasdelaalhambra/MapaFragment.kt
@@ -12,6 +12,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.addCallback
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
@@ -30,6 +31,11 @@ class MapaFragment : Fragment(), GoogleMap.OnInfoWindowClickListener, OnMapReady
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val callback = requireActivity().onBackPressedDispatcher.addCallback(this){
+            System.exit(-1)
+        }
+
     }
 
 
