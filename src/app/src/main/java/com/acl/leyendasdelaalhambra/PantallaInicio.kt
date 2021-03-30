4ed12a5496863e7f42aa121b5dd161d362176a62
Antonio Cuadros <antculap@gmail.com>
2021-03-30 12:27:58 +0200

    Avanza #38 ya que añadido diseño fragmento pantalla inicio

--- src/app/src/main/java/com/acl/leyendasdelaalhambra/PantallaInicio.kt
+++ src/app/src/main/java/com/acl/leyendasdelaalhambra/PantallaInicio.kt
@@ -5,19 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class PantallaInicio : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         var vista = inflater.inflate(R.layout.fragment_pantalla_inicio, container, false)
+
+        val view = requireActivity().findViewById<View>(R.id.menu_inferior)
+
+        view.visibility = View.GONE
 
         return vista
     }
