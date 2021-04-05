55f79065bf8fbcaa119237af38ce517b289fde16
Antonio Cuadros <antculap@gmail.com>
2021-04-05 19:49:15 +0200

    Avanza #46, añadidos fragments y elemento en menú para dirigir a ajustes

---
+++ src/app/src/main/java/com/acl/leyendasdelaalhambra/ajustesFragment.kt
@@ -0,0 +1,22 @@
+package com.acl.leyendasdelaalhambra
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class ajustesFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ajustes, container, false)
+    }
+
+}
